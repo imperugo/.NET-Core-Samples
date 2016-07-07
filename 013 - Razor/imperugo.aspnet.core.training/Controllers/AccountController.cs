007db5e1a412e2f9b0afd0225db243c2ee7be798
gaggiga <gabriele.gaggi@brain-sys.it>
2016-07-07 19:52:51 +0200

    Aggiunta gestione login e logout

---
+++ 013 - Razor/imperugo.aspnet.core.training/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using imperugo.aspnet.core.training.Models;
+using imperugo.aspnet.core.training.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imperugo.aspnet.core.training.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<User> signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
+        {
+            var model = new LoginVM { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                        Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
