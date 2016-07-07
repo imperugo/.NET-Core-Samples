b4d7738b5efd153413698e7e9fa0e590790afc7b
gaggiga <gabriele.gaggi@brain-sys.it>
2016-07-07 20:59:59 +0200

    Added post creation and list view

--- 013 - Razor/imperugo.aspnet.core.training/Controllers/HomeController.cs
+++ 013 - Razor/imperugo.aspnet.core.training/Controllers/HomeController.cs
@@ -3,26 +3,68 @@ using imperugo.aspnet.core.training.Repositories;
 using imperugo.aspnet.core.training.Repositories.Abstracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using imperugo.aspnet.core.training.ViewModels;
+using imperugo.aspnet.core.training.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace imperugo.aspnet.core.training.Controllers
 {
-	public class HomeController : Controller
+    [Authorize]
+    public class HomeController : Controller
 	{
 		private readonly IBlogConfigurationRepository blogConfigurationRepository;
+        private IBlogPostRepository blogPostRepository;
 
-		public HomeController(IBlogConfigurationRepository blogConfigurationRepository) {
+        public HomeController(IBlogConfigurationRepository blogConfigurationRepository, 
+            IBlogPostRepository blogPostRepository) {
 			this.blogConfigurationRepository = blogConfigurationRepository;
-		}
+            this.blogPostRepository = blogPostRepository;
+        }
 
+        [AllowAnonymous]
 		public IActionResult Index()
 		{
 			var model = this.blogConfigurationRepository.GetDefaultConfiguration();
 			return View(model);
 		}
 
-		public void Throw()
+        [AllowAnonymous]
+        public IActionResult List()
+        {
+            var model = this.blogPostRepository.GetPosts(0, 10);
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public void Throw()
 		{
 			int.Parse("this string is not a number so, an Exception should be throw");
 		}
-	}
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View(new PostEditVM());
+        }
+
+        [HttpPost]
+        public IActionResult Create(PostEditVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var post = new Post();
+                post.Slug = model.Slug;
+                post.Title = model.Title;
+                post.Abstract = model.Abstract;
+                post.Body = model.Body;
+                post.PublishDate = model.PublishDate;
+                this.blogPostRepository.Add(post);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+    }
 }
---
+++ 013 - Razor/imperugo.aspnet.core.training/ViewModels/PostEditVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imperugo.aspnet.core.training.ViewModels
+{
+    public class PostEditVM
+    {
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTimeOffset PublishDate { get; set; }
+    }
+}
